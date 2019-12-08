bd24b68de791a77917fb9d34a491106441f9168c
Hilal Eren <hilalerenn@icloud.com>
2019-12-08 23:38:08 +0300

    stm32f4xx_hal_msp.c

--- stm32f4xx_hal_msp.c
+++ stm32f4xx_hal_msp.c
@@ -99,7 +99,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
 
   /* USER CODE END TIM1_MspInit 1 */
   }
-  else if(htim_base->Instance==TIM2)
+  else if(htim_base->Instance==TIM2) 
   {
   /* USER CODE BEGIN TIM2_MspInit 0 */
 
